67cc297d91c3cc34178ff8743d841510cc4026f6
Yupo <yupotown@gmail.com>
2016-07-02 00:05:11 +0900

    グリッドとコマを少し大きくした

--- MorrisSiv3D/Main.cpp
+++ MorrisSiv3D/Main.cpp
@@ -8,24 +8,43 @@ void Main()
 {
 	Morris game;
 
-	// 描画位置や色
+	// 盤面を描画する中心の位置
 	Point center = Window::Size() / 2;
-	Size grid = { 100, 100 };
+
+	// 盤面の1マスを描画する大きさ
+	Size grid = { 120, 120 };
+
+	// グリッドの色
+	Color colLine = { 255, 255, 255, 255 };
+
+	// グリッドの各点の座標
+	std::function<Point(int, int)> gridPos = [&](int x, int y) {
+		return Point(center.x + (x - 1) * grid.x, center.y + (y - 1) * grid.y);
+	};
+
+	// 置く前のコマを描画する位置
 	Point posP[2][3];
 	for (int i = 0; i < 2; ++i) for (int j = 0; j < 3; ++j) {
 		posP[i][j] = { center.x + (4 * i - 2) * grid.x, center.y + (j - 1) * grid.y };
 	}
-	double rp = 20;
-	Color
-		colP[2] = { {255, 0, 0, 255}, {0, 0, 255, 255} },
-		colHolding[2] = { {255, 0, 0, 128}, {0, 0, 255, 128} },
-		colLine = { 255, 255, 255, 255 };
+
+	// コマを描画する半径
+	double rp = 24;
+
+	// コマの色
+	Color colP[2] = { {255, 0, 0, 255}, {0, 0, 255, 255} };
+
+	// 掴まれているコマを元の場所に描画するときの色
+	Color colHolding[2] = { {255, 0, 0, 128}, {0, 0, 255, 128} };
+
+	// 番や勝ち負けを表示するフォント
 	Font fontTurn(20), fontWin(20);
+
+	// 番や勝ち負けを表示する位置
 	Point posTurn = { 10, 10 };
+
+	// 番屋勝ち負けを表示する色
 	Color colTurn = { 255, 255, 255, 255 }, colWin = { 255, 255, 255, 255 };
-	std::function<Point(int, int)> gridPos = [&](int x, int y) {
-		return Point(center.x + (x - 1) * grid.x, center.y + (y - 1) * grid.y);
-	};
 
 	// 掴んでいるコマ
 	MorrisState::Player holdingPlayer = MorrisState::first;
